9d45fc01191ff773f1859ee4fb98872768088f48
vincentkua <zylyn89@gmail.com>
2024-08-28 22:31:52 +0800

    feat: added react bootstrap and some basic ui update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,23 @@
+import { Button } from "react-bootstrap";
+import logo512 from "/512.png";
+
 const Home = () => {
   return (
     <>
-      <h3>Oreo Pika</h3>
-      <p>Oreo.Pika is a cat!!!!</p>
+      <img
+        src={logo512}
+        style={{
+          display: "block",
+          maxWidth: "300px",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      />
+      <div style={{textAlign:"center"}}>
+        <h3>Oreo Pika</h3>
+        <p>Oreo.Pika is a cat!!!!</p>
+        <Button>Says Meow</Button>
+      </div>
     </>
   );
 };
